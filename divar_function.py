3279687d6bd3069ed5ff2e98dda101428d796e3a
hossein khakpoor <mhosseinkhakpoor@gmail.com>
2026-02-02 18:49:39 +0330

    setep 3

--- divar_function.py
+++ divar_function.py
@@ -1,4 +1,4 @@
-import requests,uuid
+import requests,uuid,tabulate,time
 def get_number(token,authorization):
     contact_uuid = str(uuid.uuid4())
     url=f'https://api.divar.ir/v8/postcontact/web/contact_info_v2/{token}'
@@ -41,9 +41,9 @@ def serch_query(quary,city):
     serche0=requests.post(url,json=data,headers=headers)
     if serche0.status_code != 200:
         return []
-    table = []
+    t = []
     for s in serche0.json()["suggestions"]:
-        table.append((
+        t.append((
             s["title"],
             s["subtitle"],
             s["search_data"]["query"],
@@ -51,4 +51,51 @@ def serch_query(quary,city):
             int(s["ad_count"]),
             s["probability"]
         ))
+    headers = ["title", "subtitle","query","value","ad_count","probability"]
+    table=tabulate.tabulate(t, headers=headers, tablefmt="grid")
     return table
+
+def ads_export(quary,city,category,last_post_date=''):
+    url='https://api.divar.ir/v8/postlist/w/search'
+    data={"city_ids":city,
+          "last_post_data": last_post_date,
+       "source_view":"SEARCH_BAR_QUERY_SUGGESTION",
+       "disable_recommendation":'false',
+       "map_state":{"camera_info":{"bbox":{}}},
+       "search_data":{"form_data":{"data":{"category":{"str":{"value":category}}}},
+                      "server_payload":{"@type":"type.googleapis.com/widgets.SearchData.ServerPayload",
+                                        "additional_form_data":{"data":{"sort":{"str":{"value":"sort_date"}}}}},
+                                        "query":quary}}
+    headers={"content-type":"application/json"}
+    serche1=requests.post(url,json=data,headers=headers)
+    tokens=[]
+    City=[]
+    for s in serche1.json()['list_widgets']:
+        try:
+            City.append(s["data"]["action"]["payload"]["web_info"]["city_persian"])
+            tokens.append(s["data"]["action"]["payload"]["token"])
+        except:
+            pass
+    serche_data={
+        "last_post_date":serche1.json()['pagination']['data']['last_post_date'],
+        "tokens": tokens,
+        "City": City
+    }
+    return serche_data 
+
+def ads_export_all(quary,city,category,number_of_ads=24):
+    data_serch=ads_export(quary,city,category,last_post_date='')
+    last_post_date=data_serch['last_post_date']
+    City=data_serch['City']
+    tokens=data_serch['tokens']
+    while len(tokens)<number_of_ads:
+        time.sleep(5)
+        data_serch=ads_export(quary,city,category,last_post_date)
+        last_post_date=data_serch['last_post_date']
+        City=City+data_serch['City']
+        tokens=tokens+data_serch['tokens']
+    searche_data={
+        "tokens": tokens,
+        "City": City
+    }
+    return searche_data
--- user_cruling.py
+++ user_cruling.py
@@ -6,5 +6,9 @@ phone_number_owner=os.getenv("phone_number_owner01")
 token='gZn4ITPT'
 #phon=divar_function.get_number(token=token,authorization=authorization)
 #print(phon)
-d=divar_function.serch_query('یدک کش',['715'])
-print(d)
+#d=divar_function.serch_query('یدک کش',['715'])
+#print(d)
+quary='یدکش'
+city=['715']
+category="transport"
+print(divar_function.ads_export(quary,city,category,last_post_date=''))
