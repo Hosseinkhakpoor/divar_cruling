22e9e5d77a685e537b37063845c45d088cf1a8a6
hossein khakpoor <mhosseinkhakpoor@gmail.com>
2026-02-07 17:20:59 +0330

    step4

--- divar_function.py
+++ divar_function.py
@@ -55,21 +55,47 @@ def serch_query(quary,city):
     table=tabulate.tabulate(t, headers=headers, tablefmt="grid")
     return table
 
-def ads_export(quary,city,category,last_post_date=''):
+def ads_export(quary,city,category,last_post_date='',layer_page=1,page=1,first_page_viewed_at='',search_uid=''):
     url='https://api.divar.ir/v8/postlist/w/search'
-    data={"city_ids":city,
-          "last_post_data": last_post_date,
-       "source_view":"SEARCH_BAR_QUERY_SUGGESTION",
-       "disable_recommendation":'false',
-       "map_state":{"camera_info":{"bbox":{}}},
-       "search_data":{"form_data":{"data":{"category":{"str":{"value":category}}}},
-                      "server_payload":{"@type":"type.googleapis.com/widgets.SearchData.ServerPayload",
-                                        "additional_form_data":{"data":{"sort":{"str":{"value":"sort_date"}}}}},
-                                        "query":quary}}
-    headers={"content-type":"application/json"}
+    data0={"city_ids": city,
+            "source_view": "SEARCH",
+            "disable_recommendation": "false",
+            "map_state": {"camera_info": {"bbox": {}}},
+            "search_data": {"form_data": {"data": {"category": {"str": {"value": category}}}},
+                "server_payload": {
+                "@type": "type.googleapis.com/widgets.SearchData.ServerPayload",
+                "additional_form_data": {"data": {"sort": {"str": {"value": "sort_date"}}}}},
+                "query": quary}}
+    data1={"city_ids": city,
+            "source_view": "SEARCH",
+            "pagination_data": {
+                "@type": "type.googleapis.com/post_list.PaginationData",
+                "last_post_date": last_post_date,
+                "page": page,
+                "layer_page": layer_page,
+                "search_uid": search_uid,
+                "first_page_viewed_at": first_page_viewed_at},
+            "disable_recommendation": "false",
+            "map_state": {"camera_info": {"bbox": {}}},
+            "search_data": {
+                "form_data": {
+                "data": {"category": {"str": {"value": category}}}},
+                "server_payload": {
+                "@type": "type.googleapis.com/widgets.SearchData.ServerPayload",
+                "additional_form_data": {"data": {"sort": { "str": {"value": "sort_date"}}}}},
+                "query":quary}}
+    if last_post_date=='':
+        data=data0
+    else:
+        data=data1
+    headers={"content-type":"application/json",
+             "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
     serche1=requests.post(url,json=data,headers=headers)
     tokens=[]
     City=[]
+    if serche1.status_code != 200:
+        print(f"Error: {serche1.status_code} - {serche1.text}")
+        return []        
     for s in serche1.json()['list_widgets']:
         try:
             City.append(s["data"]["action"]["payload"]["web_info"]["city_persian"])
@@ -78,20 +104,30 @@ def ads_export(quary,city,category,last_post_date=''):
             pass
     serche_data={
         "last_post_date":serche1.json()['pagination']['data']['last_post_date'],
+        "first_page_viewed_at": serche1.json()['pagination']['data']['first_page_viewed_at'],
+        "layer_page":serche1.json()['pagination']['data']['layer_page'],
+        "page":serche1.json()['pagination']['data']['page'],
+        "search_uid":serche1.json()['pagination']['data']['search_uid'],
         "tokens": tokens,
         "City": City
     }
     return serche_data 
 
 def ads_export_all(quary,city,category,number_of_ads=24):
-    data_serch=ads_export(quary,city,category,last_post_date='')
+    data_serch=ads_export(quary,city,category)
     last_post_date=data_serch['last_post_date']
+    page=data_serch['page']
+    layer_page=data_serch['layer_page']
+    first_page_viewed_at=data_serch['first_page_viewed_at']
+    search_uid=data_serch['search_uid']
+    print(last_post_date)
     City=data_serch['City']
     tokens=data_serch['tokens']
     while len(tokens)<number_of_ads:
         time.sleep(5)
-        data_serch=ads_export(quary,city,category,last_post_date)
+        data_serch=ads_export(quary,city,category,last_post_date,layer_page,page,first_page_viewed_at,search_uid)
         last_post_date=data_serch['last_post_date']
+        print(last_post_date)
         City=City+data_serch['City']
         tokens=tokens+data_serch['tokens']
     searche_data={
